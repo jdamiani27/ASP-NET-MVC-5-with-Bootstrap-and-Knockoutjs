6d1c640f80c1a5bbea78603ae799d99ecd586d5f
Jason Damiani <me@jasondamiani.com>
2015-06-17 23:32:17 -0400

    Completed through Sorting the Authors

--- BootstrapIntroduction/BootstrapIntroduction/Global.asax.cs
+++ BootstrapIntroduction/BootstrapIntroduction/Global.asax.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using BootstrapIntroduction.DAL;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -16,6 +18,10 @@ namespace BootstrapIntroduction
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            var bookContext = new BookContext();
+            Database.SetInitializer(new BookInitializer());
+            bookContext.Database.Initialize(true);
         }
     }
 }
